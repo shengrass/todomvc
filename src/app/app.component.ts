23cdbe76d21187d06b73a56aa9b65681a17cd17c
shengrass <shengrass@gmail.com>
2017-04-19 15:35:22 +0800

    任務 14：注入 Http 服務元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Http } from '@angular/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -11,6 +12,11 @@ export class AppComponent {
   todos: any[] = [];
   todo: string = '';
   filterType: string = 'all';
+  isSelectAll = false;
+
+  constructor(private http: Http) {
+
+  }
   addTodo() {
     if (this.todo) {
       //方法1 "..."this.todos 是用來展開 array
@@ -29,5 +35,30 @@ export class AppComponent {
 
   switchType(ft: string) {
     this.filterType = ft;
+  }
+
+  toggleAll() {
+    if (this.isSelectAll) {
+      this.todos.forEach(item => {
+        item.done = true;
+      });
+    }
+    else {
+      this.todos.forEach(item => {
+        item.done = false;
+      });
+    }
+  }
+  checkToggle() {
+    console.log("checkToggle");
+    const length = this.todos.filter(item => item.done === this.isSelectAll).length;
+    this.isSelectAll = length > 0 ? false : true;
+  }
+
+  removeItem(item) {
+    const index = this.todos.indexOf(item);
+    //從Array移除元素
+    this.todos.splice(index, 1);
+    this.todos = [...this.todos];
   }
 }
