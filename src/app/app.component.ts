e42bd5df1708e04153ba3f570e61fd7df095eba9
shengrass <shengrass@gmail.com>
2017-04-19 10:39:35 +0800

    新增一個 table 在頁面中，試著用屬性繫結將 colspan 修改為從 AppComponent 元件中取得。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    inputHint='What needs to be done?';
+    inputHint = 'What needs to be done?';
+    col = 2;
 }
